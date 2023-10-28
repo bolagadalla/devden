61c48c31b8e294d885c89003a74730ef832b2838
Bola Gadalla <bolagadalla777@gmail.com>
2023-10-28 00:11:37 -0400

    Adding a zipping functionality to be used to zip and unzip local templates

---
+++ zipper.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Zip(source string, destination string, name string) error {
+	log.Println("Zipping your template")
+	archive, err := os.Create(fmt.Sprintf("%s/%s.zip", destination, name))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer archive.Close()
+
+	zipWriter := zip.NewWriter(archive)
+	defer zipWriter.Close()
+
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Creating a local file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		header.Method = zip.Deflate
+
+		header.Name, err = filepath.Rel(filepath.Dir(source), path)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		// Create the writer for the file header and save the content of the file
+		headerWriter, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		archive, err = os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer archive.Close()
+
+		_, err = io.Copy(headerWriter, archive)
+		return err
+	})
+}
+
+func Unzip(source string, destination string) error {
+	reader, err := zip.OpenReader(source)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	destination, err = filepath.Abs(destination)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range reader.File {
+		err := UnzipFile(f, destination)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func UnzipFile(f *zip.File, destination string) error {
+	filePath := filepath.Join(destination, f.Name)
+	if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
+		return fmt.Errorf("Couldn't find the archive anymore at path: %s", filePath)
+	}
+
+	if f.FileInfo().IsDir() {
+		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		return err
+	}
+
+	destinationFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return err
+	}
+
+	defer destinationFile.Close()
+
+	// Unzip content
+	zippedFile, err := f.Open()
+	if err != nil {
+		return err
+	}
+
+	defer zippedFile.Close()
+
+	if _, err := io.Copy(destinationFile, zippedFile); err != nil {
+		return err
+	}
+	return nil
+}
